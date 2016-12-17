83cb1bc179d7c18b78515b1fb7f702947f40afa5
Marlon Alagoda <marlon@alagoda.at>
2016-12-17 21:22:32 +0100

    implemented methods and wrote many tests

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const port = 8080;
 const express = require('express');
 const app = express();
 const redis = require('redis');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const hash = require('string-hash');
+const async = require('async');
 
 /*
  * Init
@@ -23,47 +24,140 @@ app.use(bodyParser.json());
  * Logic
  */
 const createNewEvent = (req, res) => {
-    const name = req.body.name;
-    const startDate = req.body.startDate;
-    const duration = req.body.duration;
+	const name = req.body.name;
+	const startDate = req.body.startDate;
+	const duration = req.body.duration;
 
-    if(!name || !startDate || !duration){
-        res.status(400).send('Parameter missing');
-        return;
-    }
+	if(!name || !startDate || !duration){
+		return res.status(400).send('Parameter missing');
+	}
 
-    const id = hash(name);
+	const id = hash(name);
 
-    redisClient.get(id, (err, event) => {
-        if(!event){
-            redisClient.set(id, JSON.stringify({name: name, startDate: startDate, duration: duration}), (err) => {
-                if(err) {
-                    console.error(`Error while creating Event with id ${id}.`);
-                    res.status(500).send();
-                }
+	redisClient.get(id, (err, event) => {
+		if(err) {
+			console.error(`Error while creating Event with id ${id}.`);
+			return res.status(500).send();
+		}
 
-                res.status(200).send('OK');
-            });
-        } else {
-            res.status(400).send('Event already exists');
-        }
-    });
+		if(!event){
+			redisClient.set(id, JSON.stringify({name: name, startDate: startDate, duration: duration}), (err) => {
+				if(err) {
+					console.error(`Error while creating Event with id ${id}.`);
+					return res.status(500).send();
+				}
+
+				return res.status(200).send(String(id));
+			});
+		} else {
+			return res.status(400).send('Event already exists');
+		}
+	});
 };
 
 const editEvent = (req, res) => {
+	const id = req.params.id;
 
+	if(!id) {
+		return res.status(400).send('Parameter missing');
+	}
+
+	redisClient.get(id, (err, event) => {
+		if(err) {
+			console.error(`Error while loading Event with id ${id}.`);
+			return res.status(500).send();
+		}
+
+		if(!event){
+			return res.status(404).send();
+		}
+
+		event = JSON.parse(event);
+
+		const name = req.body.name || event.name;
+		const startDate = req.body.startDate || event.startDate;
+		const duration = req.body.duration || event.duration;
+
+		redisClient.set(id, JSON.stringify({name: name, startDate: startDate, duration: duration}), (err) => {
+			if(err) {
+				console.error(`Error while updating Event with id ${id}.`);
+				return res.status(500).send();
+			}
+
+			return res.status(200).send(String(id));
+		});
+	});
 };
 
 const deleteEvent = (req, res) => {
+	const id = req.params.id;
 
+	if(!id) {
+		return res.status(400).send('Parameter missing');
+	}
+
+	redisClient.get(id, (err) => {
+		if(err) {
+			console.error(`Error while loading Event with id ${id}.`);
+			return res.status(500).send();
+		}
+
+		redisClient.del(id, (err) => {
+			if(err) {
+				console.error(`Error while deleting Event with id ${id}.`);
+				return res.status(500).send();
+			}
+
+			return res.status(200).send('OK');
+		});
+	});
 };
 
-const getAllEvents = (req, res) => {
+const listEvents = (req, res) => {
+	redisClient.keys('*', (err, ids) => {
+		if(err){
+			console.error('Error while reading events.');
+			return res.status(500).send();
+		}
 
+		const tasks = [];
+		ids.forEach((id) => {
+			tasks.push((cb) => {redisClient.get(id, cb);});
+		});
+
+		async.parallel(
+            tasks,
+            (err, events) => {
+	if(err){
+		console.error('Error while reading events.');
+		return res.status(500).send();
+	}
+
+	return res.status(200).send(events);
+}
+        );
+	});
 };
 
 const getEvent = (req, res) => {
+	const id = req.params.id;
 
+	if(!id) {
+		return res.status(400).send('Parameter missing');
+	}
+
+	redisClient.get(id, (err, event) => {
+		if(err) {
+			console.error(`Error while loading Event with id ${id}.`);
+			return res.status(500).send();
+		}
+
+		if(!event){
+			return res.status(404).send();
+		}
+
+		return res.status(200).send(event);
+	});
 };
 
 /*
@@ -72,14 +166,14 @@ const getEvent = (req, res) => {
 app.post('/event', createNewEvent);
 app.post('/event/:id', editEvent);
 app.delete('/event/:id', deleteEvent);
-app.get('/events', getAllEvents);
+app.get('/events', listEvents);
 app.get('/event/:id', getEvent);
 
 /*
  * Start app
  */
 app.listen(port, () => {
-    console.log(`Event Service running on ${port}`)
+	console.log(`Event Service running on ${port}`);
 });
 
 module.exports = app;
---
+++ test.js
@@ -0,0 +1,191 @@
+const redis = require('redis');
+const request = require('supertest');
+const assert = require('assert');
+const fakeredis = require('fakeredis');
+const sinon = require('sinon');
+const randomstring = require('randomstring');
+const async = require('async');
+const hash = require('string-hash');
+
+describe('Event Service', () => {
+	var redisClient;
+	var eventService;
+
+	before(() => {
+        // Mock redis
+		sinon.stub(redis, 'createClient', fakeredis.createClient);
+		eventService = require('./index');
+		redisClient = redis.createClient('eventService');
+	});
+
+	afterEach((done) => {
+		redisClient.flushdb((err) => {
+			assert(!err, err ? err.message : '');
+			done();
+		});
+	});
+
+	describe('createNewEvent', () => {
+		it('should not save new event, since name has to be unique', (done) => {
+			const name = 'testevent';
+			const id = hash(name);
+
+			redisClient.set(id, 'xx', () => {
+				request(eventService)
+                .post('/event')
+                .set('Content-Type', 'application/x-www-form-urlencoded')
+                .send({name: name, startDate: 'ya', duration: 'asdf'})
+                .expect(400, (err) => {
+	assert(!err, err ? err.message : '');
+
+	done();
+});
+			});
+		});
+
+		it('should save a new event to DB', (done) => {
+			const name = 'xyzEvent';
+			const startDate = 'akljf';
+			const duration = 'LJKd';
+
+			request(eventService)
+            .post('/event')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({name: name, startDate: startDate, duration: duration})
+            .expect(200, (err, res) => {
+	assert(!err, err ? err.message : '');
+	assert.notEqual(res.text, null);
+
+	redisClient.get(res.text, (err, event) => {
+		assert(!err, err ? err.message : '');
+		assert.notEqual(event, null);
+
+		event = JSON.parse(event);
+		assert.equal(event.name, name);
+		assert.equal(event.startDate, startDate);
+		assert.equal(event.duration, duration);
+
+		done();
+	});
+});
+		});
+	});
+
+	describe('edit event', () => {
+		it('should update an existing event', (done) => {
+			const name = 'testevent';
+			const id = hash(name);
+
+			redisClient.set(id, JSON.stringify({name: name, startDate: 'llkasdf', duration: 'asldfkj'}), () => {
+				const newName = 'laskjf';
+				const newStartDate = 'aslkdfj';
+				const newDuration = 'sdf';
+
+				request(eventService)
+                .post(`/event/${id}`)
+                .send({name: newName, startDate: newStartDate, duration: newDuration})
+                .expect(200, (err, res) => {
+	assert(!err, err ? err.message : '');
+
+	redisClient.get(res.text, (err, event) => {
+		assert(!err, err ? err.message : '');
+		assert.notEqual(event, null);
+
+		event = JSON.parse(event);
+		assert.equal(event.name, newName);
+		assert.equal(event.startDate, newStartDate);
+		assert.equal(event.duration, newDuration);
+
+		done();
+	});
+});
+			});
+		});
+	});
+
+	describe('delete event', () => {
+		it('should delte an event', (done) => {
+			const name = 'testevent';
+			const id = hash(name);
+
+			redisClient.set(id, JSON.stringify({name: name, startDate: 'llkasdf', duration: 'asldfkj'}), () => {
+				redisClient.exists(id, (err, exists) => assert(exists));
+
+				request(eventService)
+                .delete(`/event/${id}`)
+                .expect(200, (err) => {
+	assert(!err, err ? err.message : '');
+	redisClient.exists(id, (err, exists) => assert(!exists));
+
+	done();
+});
+			});
+		});
+	});
+
+	describe('list events', () => {
+		it('should list all events', (done) => {
+			const createRandomEvent = (name, cb) => {
+				const startDate = randomstring.generate(7);
+				const duration = randomstring.generate(7);
+				const id = hash(name);
+
+				redisClient.set(id, JSON.stringify({name: name, startDate: startDate, duration: duration}), cb);
+			};
+
+			const names = ['firstName', 'secondName', 'thirdName'];
+
+			async.parallel([
+				async.apply(createRandomEvent, names[0]),
+				async.apply(createRandomEvent, names[1]),
+				async.apply(createRandomEvent, names[2])
+			], (err) => {
+				assert(!err, err ? err.message : '');
+
+				request(eventService)
+                .get('/events')
+                .expect(200, (err, res) => {
+	assert(!err, err ? err.message : '');
+	assert(res.text);
+
+	const events = JSON.parse(res.text);
+	assert.equal(Object.keys(events).length, names.length);
+
+	const eventsFound = [];
+	for(var i = 0; i < Object.keys(events).length; i++){
+		eventsFound.push(JSON.parse(events[i]).name);
+	}
+
+	const sortNames = (a, b) => a < b;
+	assert.deepEqual(names.sort(sortNames), eventsFound.sort(sortNames));
+
+	done();
+});
+			});
+
+		});
+	});
+
+	describe('get event', () => {
+		it('should return a specific event', (done) => {
+			const name = 'anyname';
+			const string = randomstring.generate(7);
+			const id = hash(name);
+
+			redisClient.set(id, JSON.stringify({name: name, startDate: string, duration: string}), (err) => {
+				assert(!err, err ? err.message : '');
+
+				request(eventService)
+                .get(`/event/${id}`)
+                .expect(200, (err, res) => {
+	assert(!err, err ? err.message : '');
+
+	const event = JSON.parse(res.text);
+	assert.deepEqual(event, {name: name, startDate: string, duration: string});
+
+	done();
+});
+			});
+		});
+	});
+});
