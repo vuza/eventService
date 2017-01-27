2aa6c271d6add187aa049d0dd0c41b962bd9ac9a
Marlon Alagoda <marlon@alagoda.at>
2017-01-27 11:44:09 +0100

    added fonts and defined all-in-one start script

--- frontend/buildTheme.js
+++ frontend/buildTheme.js
@@ -1,6 +1,7 @@
 const sass = require('node-sass');
 const fs = require('fs');
 const mkpath = require('mkpath');
+const ncp = require('ncp').ncp;
 
 var result = sass.renderSync({
   file: 'src/sass/materialize.scss',
@@ -9,5 +10,10 @@ var result = sass.renderSync({
 });
 
 mkpath.sync('build/static/css/');
-fs.writeFileSync('build/static/css/materialize.min.css');
+fs.writeFileSync('build/static/css/materialize.min.css', result.css);
 fs.createReadStream('src/scripts/materialize.min.js').pipe(fs.createWriteStream('build/static/js/materialize.min.js'));
+ncp('src/fonts', 'build/static/fonts', function (err) {
+  if (err) {
+    return console.error(err);
+  }
+});
